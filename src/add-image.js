1f005d68084b07e349e359a874d363c2abf4a4c4
Viktor Pyskunov <vpyskunov@gmail.com>
2021-01-30 13:15:01 +0200

    Remove comment

--- src/add-image.js
+++ src/add-image.js
@@ -2,7 +2,6 @@ import Kiwi from './kiwi.jpg';
 import altText from './altText.txt';
 
 function addImage() {
-    // Inside this function I will create an img dom element, specify an alt , width , and src properties.
     const img = document.createElement('img');
     img.alt = altText;
     img.width = 300;
